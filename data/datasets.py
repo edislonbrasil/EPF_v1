33cc5121e2f84ec0e55541e1960e54dadfec09c5
edislonbrasil <71200215+edislonbrasil@users.noreply.github.com>
2022-09-26 14:50:50 -0300

    Update datasets.py

--- data/datasets.py
+++ data/datasets.py
@@ -2,19 +2,19 @@ import pandas as pd
 import numpy as np
 
 def tira_repetidos(dados_brutos)
-  """ Função para retirar os indices repetidos
-  Por conta do horário de verão, em todos os anos tem um indice repetido 
-  correspondendo a passagem do horario de verao
+  # Função para retirar os indices repetidos
+  # Por conta do horário de verão, em todos os anos tem um indice repetido 
+  # correspondendo a passagem do horario de verao
   
-  Entrada
-  dados_brutos -> descrição: dados com indices repitidos
-                  tipo: Pandas Dataframe
-                  Origem: Esses dados provem da ENTSO-E transparency platform
+  # Entrada
+  # dados_brutos -> descrição: dados com indices repitidos
+  #                 tipo: Pandas Dataframe
+  #                 Origem: Esses dados provem da ENTSO-E transparency platform
   
-  Saida
-  dados_sr -> descrição: dados sem repeticao
-              tipo: Pandas Dataframe
-  """
+  # Saida
+  # dados_sr -> descrição: dados sem repeticao
+  #             tipo: Pandas Dataframe
+  
   
   indice_rep = np.zeros(shape=(0, 0),dtype=int)
   for i in range(dados_brutos.shape[0]-1):

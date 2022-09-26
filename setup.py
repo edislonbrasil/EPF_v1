0588b0d6bbdcd8f85754a65485ba54c585f65e4a
edislonbrasil <71200215+edislonbrasil@users.noreply.github.com>
2022-09-26 15:36:00 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'EPF_v1',
+    name = 'epfv1',
     version = '0.1.0',
-    url = 'https://github.com/edislonbrasil/EPF_v1',
+    url = 'https://github.com/edislonbrasil/epfv1',
     description = 'Tools for Electricity Price Forecasting',
-    packages = find_packages(include=['EPF_v1', 'EPF_v1.*']),
+    packages = find_packages(include=['epfv1', 'epfv1.*']),
     install_requires = ['pandas>=1', 'numpy>=1'
       ]
 )
